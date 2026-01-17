f9228e987e38276fe22b4647b3dfedcfeaedb66c
maziathecoder <bajiwa@gmail.com>
2026-01-17 11:04:42 +0500

    v1

--- basic pygames/main.py
+++ basic pygames/main.py
@@ -1,13 +1,24 @@
-import pygame  
-  
-pygame.init()  
-screen = pygame.display.set_mode((400, 300))  
-done = False  
-  
-while not done:  
-    for event in pygame.event.get():  
-        if event.type == pygame.QUIT:  
-            done = True  
-    pygame.draw.rect(screen, (0, 125, 255), pygame.Rect(30, 30, 60, 60))    
-  
-    pygame.display.flip()  
+import pygame
+
+pygame.init()
+# Create the display surface object of specific dimension.
+window = pygame.display.set_mode((400, 400))
+# Fill the screen with white color
+window.fill((255, 255, 255))
+# Define colors
+GREEN = (0, 255, 0)
+# Draw solid circle
+pygame.draw.circle(window, GREEN, (300, 300), 50)
+# Draw outlined circle
+pygame.draw.circle(window, GREEN, (100, 100), 50, 3)
+# Draws the surface object to the screen.
+pygame.display.update()
+# Game loop
+running = True
+while running:
+    # Event handling
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+# Quit pygame
+pygame.quit()
